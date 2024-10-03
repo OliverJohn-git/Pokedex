157429cb9b28d0afb9367cd232a55c32f11c476a
OliverJohn-git <johnoliver846@gmail.com>
2024-10-03 23:57:13 +0200

    poke cards and rendering

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 function init(){
-    loadApi();
-    render();
-}
-let limit=0
-
-async function loadApi(limit) {
-    let pokeApi = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`)
-    limit=20
+    loadPokeApi();
 }
 
-function render(pokeApi, limit){
-    for (let i = 1; i == limit; i++) {
-        const singlePokemon = pokeApi [i];
-        console.log(singlePokemon)
-    }
+async function loadPokeApi() {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`);
+    let pokeApi = await response.json();
+
+    console.log(pokeApi);
+
+    renderPokeCard(pokeApi);
 }
+
+async function renderPokeCard(pokeApi){
+    let content = document.getElementById('content');
+    content.innerHTML="";
+
+    for (let pokemonI = 0; pokemonI < pokeApi.count; pokemonI++) {
+        let singlePokemon = await singlePokemonData(pokeApi.results[pokemonI].url);
+        let pokeTypeOne = singlePokemon.types[0].type.name.charAt(0).toUpperCase() + singlePokemon.types[0].type.name.slice(1);
+        let pokeTypeTwo = singlePokemon.types.length > 1 ? singlePokemon.types[1].type.name.charAt(0).toUpperCase() + singlePokemon.types[1].type.name.slice(1): null;
+        
+        pokemonCardTemp(pokeApi, pokemonI, singlePokemon, pokeTypeOne, pokeTypeTwo);
+    };
+    
+}
+
+function pokemonCardTemp(pokeApi, pokemonI, singlePokemon, pokeTypeOne, pokeTypeTwo){
+    content.innerHTML += /*html*/`
+        <div class="singlePokemonCard">
+            <h1>${pokeApi.results[pokemonI].name.charAt(0).toUpperCase() + pokeApi.results[pokemonI].name.slice(1) }</h1>;
+            <img class="singlePokemonImg" src="${singlePokemon.sprites.front_default}">
+            <div class="pokemonTypes">
+                <div>${pokeTypeOne}</div>
+                ${pokeTypeTwo? `<div>${pokeTypeTwo}</div>` : ''}
+            </div>
+        </div>
+    `
+}
+
+ async function singlePokemonData(url) {
+    let singlePokemonResponse = await fetch(url);
+    let singlePokemonApi = await singlePokemonResponse.json();
+    return singlePokemonApi;
+ }
